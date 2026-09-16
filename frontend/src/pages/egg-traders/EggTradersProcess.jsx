26e4f6083f1814793709ce442b18bddaf65864f1
Saad <mwasaad47@gmail.com>
2026-09-17 01:38:24 +0500

    Use B-g.png in internal website Process tab hero section background

--- frontend/src/pages/egg-traders/EggTradersProcess.jsx
+++ frontend/src/pages/egg-traders/EggTradersProcess.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from 'react';
 import { useCMSStore } from '../../store/useCMSStore';
 import EggTradersPageBanner from '../../components/egg-traders/EggTradersPageBanner';
 
-const HERO_IMAGE = '/images/client-final/White.png';
+const HERO_IMAGE = '/images/B-g.png';
 
 const icons = {
   Farm: (
